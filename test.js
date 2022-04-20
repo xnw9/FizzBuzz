adefa393ddcbd5cc5ae0038450e02045a5f8fa02
Sydney Wu <xinni.wu@softwire.com>
2022-04-20 13:57:10 +0100

    Made test.js quicker

--- test.js
+++ test.js
@@ -1,18 +1,24 @@
-for (let i = 0; i <= 100; i++) {
+for (let i = 1; i <= 100; i++) {
     outputValue = i.toString()
 
+
+    if (i % 5 == 0) {
+        outputValue = "Buzz"
+
+
+        if ((i/5)%3 == 0) {
+            outputValue = "FizzBuzz"
+            console.log(outputValue)
+            continue
+        }
+
+    }
 
     if (i % 3 == 0) {
         outputValue = "Fizz"
     }
 
-    if (i % 5 == 0) {
-        outputValue = "Buzz"
-    }
-    
-    if (i % 15 == 0) {
-        outputValue = "FizzBuzz"
-    }
+    /* A liiiiitle bit quicker? */
 
     console.log((outputValue))
 
