be6783cebb9486849175a00a57a6c66fcd9ac865
Sydney Wu <xinni.wu@softwire.com>
2022-04-20 13:45:44 +0100

    Update test.js

--- test.js
+++ test.js
@@ -1,4 +1,19 @@
 for (let i = 0; i <= 100; i++) {
-    console.log((i))
+    outputValue = i.toString()
+
+
+    if (i % 3 == 0) {
+        outputValue = "Fizz"
+    }
+
+    if (i % 5 == 0) {
+        outputValue = "Buzz"
+    }
+    
+    if (i % 15 == 0) {
+        outputValue = "FizzBuzz"
+    }
+
+    console.log((outputValue))
 
 }
