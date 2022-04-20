de797037386341f41dc392e2fda4fa77bac21f12
Sydney Wu <xinni.wu@softwire.com>
2022-04-20 14:01:55 +0100

    Continue updateing

--- test.js
+++ test.js
@@ -1,25 +1,29 @@
-for (let i = 1; i <= 100; i++) {
-    outputValue = i.toString()
+function fizzbuzz() {
+    for (let i = 1; i <= 100; i++) {
+        outputValue = i.toString()
 
 
-    if (i % 5 == 0) {
-        outputValue = "Buzz"
+
+        if (i % 5 == 0) {
+            outputValue = "Buzz"
 
 
-        if ((i/5)%3 == 0) {
-            outputValue = "FizzBuzz"
-            console.log(outputValue)
-            continue
+            if ((i / 5) % 3 == 0) {
+                outputValue = "FizzBuzz"
+                console.log(outputValue)
+                continue
+            }
+
         }
 
+        if (i % 3 == 0) {
+            outputValue = "Fizz"
+        }
+
+        /* A liiiiitle bit quicker? */
+
+        console.log((outputValue))
+
     }
-
-    if (i % 3 == 0) {
-        outputValue = "Fizz"
-    }
-
-    /* A liiiiitle bit quicker? */
-
-    console.log((outputValue))
-
 }
+fizzbuzz()
